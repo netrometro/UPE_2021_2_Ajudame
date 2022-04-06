772d4fd8e7b8ce36ab1912e3edc09c453f074d5f
Ewerton Mendonça <netrometro@yahoo.com.br>
2022-04-06 16:52:59 -0300

    Controlador registrar usuário INCOMPLETO

---
+++ src/main/java/br/upe/ajudame/controllers/RegisterUserController.java
@@ -0,0 +1,28 @@
+package br.upe.ajudame.controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterUserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+    public RegisterUserController() {
+        super();
+    }
+    
+	protected void doPost(
+			HttpServletRequest request, 
+			HttpServletResponse response) 
+					throws ServletException, IOException {
+		
+		
+		System.out.println("Register User");
+	
+	}
+}
