d0f27c2ec49606d7a1915ed075fbb41174750b7c
Ewerton Mendonça <netrometro@yahoo.com.br>
2022-05-04 16:27:42 -0300

    Ajuste na migration user

--- src/main/java/br/upe/ajudame/model/repositories/UserMigration.java
+++ src/main/java/br/upe/ajudame/model/repositories/UserMigration.java
@@ -1,23 +1,31 @@
 package br.upe.ajudame.model.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UserMigration {
 
-	private ConnectionPostgres repository;
+	private ConnectionPostgres postgres;
 
 	public UserMigration() {
-		repository = new ConnectionPostgres();
+		postgres = new ConnectionPostgres();
 	}
 
 	public void migrate() {
 		try {
-			repository.query("CREATE TABLE users " +
+			Connection conn = postgres.connect();
+			String sql = "CREATE TABLE users " +
 			        "(id SERIAL PRIMARY KEY ," +
 			        " name VARCHAR, " +
 			        " email VARCHAR(50), " +
 			        " password VARCHAR(50)" +
-			        ");");
+			        ");";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.execute();
+			
+			ps.close();
+			conn.close();
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
@@ -25,7 +33,11 @@ public class UserMigration {
 	
 	public void refresh() {
 		try {
-			repository.query("DROP TABLE IF EXISTS users;");
+			Connection conn = postgres.connect();
+			String sql = "DROP TABLE IF EXISTS users;";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.execute();
+			
 			this.migrate();
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
@@ -38,11 +50,21 @@ public class UserMigration {
 					{ "Fulano", "fulano@email.com", "123" },
 					{ "Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon", "pedro.alcantara@email.com", "111" }
 				};
+			Connection conn = postgres.connect();
+			String sql = "INSERT INTO users" +
+			        "  (name, email, password) VALUES " +
+			        " (?, ?, ?);";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
 			for (String[] d : data) {
-				repository.queryPrepared("INSERT INTO users" +
-				        "  (name, email, password) VALUES " +
-				        " (?, ?, ?);", d );
+				ps.setString(1, d[0]);
+				ps.setString(2, d[1]);
+				ps.setString(3, d[2]);
+				ps.execute();
 			}
+			
+			ps.close();
+			conn.close();
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
